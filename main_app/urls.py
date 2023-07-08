bb905cba383bd0cc9529b20beb92808f0dd03b69
Dolores Crazover <doloresnetweb@gmail.com>
2023-07-08 11:48:56 -0400

    urls home, login, signup done and template files created and in process of completion

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.Home.as_view(), name="home"), 
+    path('login/', views.Login.as_view(), name="login"),
+    path('signup/', views.Signup.as_view(), name="signup"),
 ]
